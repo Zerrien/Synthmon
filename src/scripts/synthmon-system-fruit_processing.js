b3b55829e7b9c3b0549edd4aa546467dfa98d984
Zerrien <josh.callebaut@gmail.com>
2015-07-24 23:37:56 -0700

    Fruit... FRUIT!
    :banana: :apple: :strawberry:

--- src/scripts/synthmon-system-fruit_processing.js
+++ src/scripts/synthmon-system-fruit_processing.js
@@ -3,3 +3,25 @@
 	Tap Deluxe
 	Timed Clicker
 */
+ECS.Scenes.FruitGame = {
+	"systems":[
+		"FruitControl",
+		"FruitLogic",
+		"FruitRender"
+	],
+	"entities":[],
+	"logic":function() {
+		for(var i = 0 ; i < this.systems.length; i++) {
+			if(this.systems[i]) {
+				this.systems[i](fruitEntities);
+			}
+		}
+	},
+	"init":function() {
+		fruitEntities = this.entities;
+		for(var i = 0; i < this.systems.length; i++) {
+			this.systems[i] = ECS.Systems[this.systems[i]];
+		}
+	}
+};
+var fruitEntities = ECS.Scenes.FruitGame.entities; //Alias
