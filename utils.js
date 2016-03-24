43f3e7a40fa0d3a2f5ace26ccfb847e5c446f24c
Zerrien <josh.callebaut@gmail.com>
2016-03-23 23:45:28 -0700

    Implements a majority of the Vector functions

--- utils.js
+++ utils.js
@@ -122,64 +122,177 @@ class Matrix {
 	}
 }
 class Vector {
-	constructor() {
-
+	constructor(x, y, z) {
+		// Probably a better way to do this
+		// Also consider Z being optional in the constructor
+		if(x instanceof Vector) {
+			this.x = x.x;
+			this.y = x.y;
+			this.z = x.z;
+		} else if (typeof x == "object") {
+			if(typeof x.x == "number" && typeof x.y == "number" && typeof x.z == "number") {
+				this.x = x.x;
+				this.y = x.y;
+				this.z = x.z;
+			} else {
+				// Probably throw and error here
+			}
+		} else if (typeof x == "number" && typeof y == "number" && typeof z == "number") {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		} else {
+			// Probably throw an error here
+		}
 	}
 	add(vec) {
-
+		// Could we turn this into add(vec | scalar)?
+		// add(1) vs add(new Vector(1, 1, 1))?
+		// Confusing?
+		if(Vector.hasXYZ(vec)) {
+			this.x += vec.x;
+			this.y += vec.y;
+			this.z += vec.z;
+			return this;
+		} else {
+			// Probably error
+		}
 	}
 	addS(scalar) {
+		if(typeof scalar == "number") {
+			this.x += scalar;
+			this.y += scalar;
+			this.z += scalar;
+			return this;
+		} else {
+			// Probably error
+		}
 
 	}
 	sub(vec) {
-
+		if(Vector.hasXYZ(vec)) {
+			this.x -= vec.x;
+			this.y -= vec.y;
+			this.z -= vec.z;
+			return this;
+		} else {
+			// Error
+		}
 	}
 	subS(scalar) {
-
+		if(typeof scalar == "number") {
+			this.x -= scalar;
+			this.y -= scalar;
+			this.z -= scalar;
+			return this;
+		} else {
+			// Error
+		}
 	}
 	divS(scalar) {
-
+		if(typeof scalar == "number") {
+			this.x /= scalar;
+			this.y /= scalar;
+			this.z /= scalar;
+			return this;
+		} else {
+			// Error
+		}
 	}
 	multS(scalar) {
-
+		if(typeof scalar == "number") {
+			this.x *= scalar;
+			this.y *= scalar;
+			this.z *= scalar;
+			return this;
+		} else {
+			// Error
+		}
 	}
 	dot(vec) {
 		// vec mult -> scalar
+		if(Vector.hasXYZ(vec)) {
+			return this.x * vec.x + this.y * vec.y + this.z * vec.z;
+		} else {
+			// Error
+		}
 	}
 	cross(vec) {
 		// vec mult -> vec
+		if(Vector.hasXYZ(vec)) {
+			// xyzzy
+			return new Vector(this.y * vec.z - this.z * vec.y, this.z * vec.x - this.x * vec.z, this.x * vec.y - this.y * vec.x);
+		} else {
+			// Error
+		}
 	}
 	normalize() {
-
+		this.divS(this.magnitude);
+		return this;
 	}
 	rotate(radians) {
-
+		// Complicated...
 	}
 	max(num) {
-
+		if(typeof num == "number") {
+			if(this.magnitude < num) {
+				this.normalize().multS(num);
+			}
+			return this;
+		} else {
+			// Error
+		}
 	}
 	min(num) {
-
+		if(typeof num == "number") {
+			if(this.magnitude > num) {
+				this.normalize().multS(num);
+			}
+			return this;
+		} else {
+			// Error
+		}
 	}
 	projectionS(b) {
 		// α = a dot bUnit
+		return this.dot(b.normal);
 	}
 	projection(b) {
 		// a1 = α * bUnit
+		return new Vector(b.normal.multS(this.projectionS(b)));
 	}
 	rejection(b) {
 		// a2 = a - a1
+		return this.copy().sub(this.projection(b));
 	}
 	copy() {
-
+		return new Vector(this);
 	}
 	get magnitude() {
-
+		return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2))
 	}
 	get normal() {
 		// .copy().normalize()
+		return this.copy().normalize();
 	}
 	get heading() {
+		// this is complicated?
+		//return Math.atan2()
 
+	}
+	static hasXYZ(vec) {
+		if(vec instanceof Vector) return true;
+		if (typeof vec.x == "number" && typeof vec.y == "number" && typeof vec.z == "number") return true;
+		return false;
+	}
+}
+class Rectangle {
+	constructor() {
+
+	}
+}
+class Circle {
+	constructor() {
+		
 	}
 }
